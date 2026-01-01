996fb5ac36652702417c1a8dfc78fa6ea57ed0d4
aima kareem <amikareem921@gmail.com>
2026-01-01 18:51:04 +0500

    rename the herosection

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
 import link from "next/link";   
-import Image from "next/image";
-import LandingCard from "../components/LandingCard";    
+import Image from "next/image";   
+import HeroSection from "../components/HeroSection";
 export default function Homepage() {
 return(
     <div className="relative h-screen">
         <Image
-            src="/assets/sage-bg.jpg"
+            src="/assets/gray-bg.jpg"
             alt="Healthy Food Background"
             fill
             style={{ objectFit: 'cover' }}
             priority
         />
-        <LandingCard />
+        <HeroSection />
     
     </div>
 )
