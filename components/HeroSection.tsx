efedd1c56c1ac2b8a302b668de39c360bb26ea04
aima kareem <amikareem921@gmail.com>
2026-02-10 19:59:44 +0500

    connect about page with hero section

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -34,7 +34,7 @@ export default function HeroSection({
                 
                 {/* Button sirf Home Page pe show hoga */}
                 {showButton && (
-                    <Link href="/">
+                    <Link href="/about">
                         <button className="mt-6 px-6 py-2 bg-olive-green text-white rounded-full hover:bg-green-900 hover:scale-105 transition-all duration-300 font-serif">
                             Start your journey
                         </button>
